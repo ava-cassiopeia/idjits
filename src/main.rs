8c172a749d7acd5462b1eca87497bbc7fb7d6aaa
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2025-09-03 16:54:02 -0600

    Add idjit validation

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct Args {
   prefix: Option<String>,
 
   /// All possible valid idjits, separated by a comma.
-  #[arg(short, long)]
+  #[arg(short, long, value_delimiter = ',')]
   idjits: Vec<String>,
 
   /// The pneumonics to use to generate valid idjits. Can be specified multiple times.
@@ -37,8 +37,22 @@ fn main() {
         .into_iter()
         .filter(|&item| item != "")
         .collect();
-    println!("Pn: {:?}", optional_parts);
+
+    for optional_part in &optional_parts {
+      let optional_idjits: Vec<&str> = optional_part.split('|').collect();
+      validate_idjits(&raw_idjits, &optional_idjits);
+    }
   }
 
   println!("Hello, world! {}, {:?}, {:?}", prefix, pneumonics, raw_idjits);
+}
+
+fn validate_idjits(idjits: &Vec<String>, possible_idjits: &Vec<&str>) {
+  for possible_idjit in possible_idjits {
+    if !idjits.contains(&String::from(*possible_idjit)) {
+      panic!("'{}' is not a valid idjit.", possible_idjit);
+    }
+  }
+
+  // otherwise all is good
 }
